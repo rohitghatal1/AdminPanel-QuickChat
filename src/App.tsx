8fa55bbb862e2ccd3e4516fb3056f615adddc6b5
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-02-21 23:24:08 +0545

    added dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./components/LoginPage";
+import Dashboard from "./components/pages/Dashboard";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="mainscreen/*"></Route>
+        <Route path="mainscreen/*">
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </>
   );
