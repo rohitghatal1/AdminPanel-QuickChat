45608fb47e8da27947255ebf41f4db5fb94d701f
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-02-22 14:59:08 +0545

    added sidebar and main screen

---
+++ src/components/MainScreen.tsx
@@ -0,0 +1,7 @@
+export default function MainScreen() {
+  return (
+    <div>
+      <h2>Main Screen</h2>
+    </div>
+  );
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,9 @@
+const Sidebar: React.FC = () => {
+  return (
+    <div>
+      <h2>QuickChat</h2>
+    </div>
+  );
+};
+
+export default Sidebar;
