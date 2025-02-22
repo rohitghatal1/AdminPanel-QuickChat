dfa9fbed6c1d27a048eb0dbdcd0e4c66f371635b
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-02-22 14:56:57 +0545

    added form adn styels

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,12 +1,14 @@
 import { Form, Input } from "antd";
 
 const LoginPage: React.FC = () => {
+  const [adminLoginForm] = Form.useForm();
+
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="w-[40rem]">
-        <h2 className="text-lg py-2">Admin Login Page</h2>
-        <div>
-          <Form>
+        <h2 className="text-lg py-2 text-center">Admin Login Page</h2>
+        <div className="rounded-md border border-gray-300 p-4 shadow-md">
+          <Form layout="vertical" form={adminLoginForm}>
             <Form.Item label="Username">
               <Input />
             </Form.Item>
