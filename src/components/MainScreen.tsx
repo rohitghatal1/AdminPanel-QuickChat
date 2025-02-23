3aa5620136d85c10d16324fbaf6ed2d42cc4a8fe
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-02-23 22:36:39 +0545

    added mainscreen

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -2,6 +2,17 @@ export default function MainScreen() {
   return (
     <div>
       <h2>Main Screen</h2>
+      <div className="flex">
+        <div>
+          <aside>
+            <h2>Sidebar</h2>
+          </aside>
+        </div>
+        <div>
+          <h2>Heder</h2>
+          <div>contente</div>
+        </div>
+      </div>
     </div>
   );
 }
