fc51c97132ce4d8ae2f6de9acc5bb66b6c2e4d93
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-02-27 20:30:50 +0545

    added sidebar with icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,25 @@
+import { LuLayoutDashboard } from "react-icons/lu";
+import { PiUsersThree } from "react-icons/pi";
+import { TiMessages } from "react-icons/ti";
+
 const Sidebar: React.FC = () => {
+  const SidebarItems = [
+    {
+      icon: <LuLayoutDashboard />,
+      label: "Dashboard",
+      link: "/dashboard",
+    },
+    {
+      icon: <PiUsersThree />,
+      label: "Users",
+      link: "/users",
+    },
+    {
+      icon: <TiMessages />,
+      label: "Messages",
+      link: "/messages",
+    },
+  ];
   return (
     <div>
       <h2>QuickChat</h2>
