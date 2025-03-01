74d4dd8308ee44e25dc81929120e5b4ee9c446e3
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-03-01 21:42:40 +0545

    updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,14 @@ import { Avatar } from "antd";
 const Header: React.FC = () => {
   return (
     <div>
-      <div>
-        <Avatar>R</Avatar>
-        Rohit Ghatal
+      <div className="flex items-center justify-between px-2">
+        <div>
+          <Avatar>R</Avatar>
+          Rohit Ghatal
+        </div>
+        <div>
+          <Avatar>R</Avatar>
+        </div>
       </div>
     </div>
   );
