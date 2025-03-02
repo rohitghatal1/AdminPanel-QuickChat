205f19c01e78976012079018117eeb24d82364cb
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-03-02 20:51:52 +0545

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./components/LoginPage";
 import Dashboard from "./components/pages/Dashboard";
+import Users from "./components/pages/Users";
+import Messages from "./components/pages/Messages";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="mainscreen/*">
           <Route path="dashboard" element={<Dashboard />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/messages" element={<Messages />} />
         </Route>
       </Routes>
     </>
