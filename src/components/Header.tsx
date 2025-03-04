ce20e593e2d333b4eb9250f0d000dd48705afe40
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-03-04 22:45:54 +0545

    added header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,87 @@
-import { Avatar } from "antd";
+import { Avatar, Badge, Button, Popover, Tag } from "antd";
+import moment from "moment";
+import { FaPlus, FaRegBell, FaSearch, FaTimes, FaUser } from "react-icons/fa";
+import { GoClock } from "react-icons/go";
+import { GrAnnounce } from "react-icons/gr";
+import { MdSupportAgent } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
     <div>
-      <div className="flex items-center justify-between px-2">
-        <div>
-          <Avatar>R</Avatar>
-          Rohit Ghatal
+      <header className="  flex items-center justify-between px-5 h-[70px] shadow-md">
+        {/* <Button onClick={() => setCollapsed(!collapsed)} className=' ' type='text'>
+						<FaBars className='text-xl' />
+					</Button> */}
+
+        <div className="flex items-center gap-1">
+          <div>
+            <Avatar
+              className="bg-gray-300 text-white mx-4 min-h-[45px] min-w-[45px] "
+              // src={businessData?.image}
+            >
+              <span className="text-[30px]"> Rohit Ghatal</span>
+            </Avatar>
+          </div>
+
+          <div className=""></div>
         </div>
-        <div>
-          <Avatar>R</Avatar>
+
+        <div className="flex items-center space-x-3  px-5">
+          <Link to={"/logs"}>
+            <Button
+              className=" flex items-center h-10 rounded-xl"
+              icon={<GoClock />}
+            >
+              Logs
+            </Button>
+          </Link>
+
+          <Popover placement="bottomRight" trigger="click">
+            <Badge>
+              <div className="rounded-full cursor-pointer p-3 text-lg bg-gray-200">
+                <FaRegBell className="text-gray-600 text-[16px]" />
+              </div>
+            </Badge>
+          </Popover>
+          <Popover placement="bottomLeft" trigger="click" className="relative">
+            <i className="text-gray-500 absolute top-[7px] left-3 translate-y-1">
+              <FaSearch />
+            </i>
+            <input
+              placeholder="Search Customers / Leads"
+              className="w-[18rem] h-10 border text-sm rounded-xl focus:outline-none pl-10"
+            />
+            {
+              <span className=" flex items-center absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer">
+                <FaTimes className="text-gray-500" />
+              </span>
+            }
+          </Popover>
+
+          <Popover
+            placement="bottomRight"
+            trigger="click"
+            // open={open}
+            // onOpenChange={() => setOpen(!open)}
+            // content={content}
+            className="flex cursor-pointer items-center gap-2"
+          >
+            <Avatar
+              className="bg-gray-300 text-white  h-[40px] w-[40px]"
+              // src={userData?.image}
+            >
+              <FaUser />
+            </Avatar>
+          </Popover>
+
+          {
+            <i className="text-4xl text-gray-600 cursor-pointer">
+              <MdSupportAgent />
+            </i>
+          }
         </div>
-      </div>
+      </header>
     </div>
   );
 };
