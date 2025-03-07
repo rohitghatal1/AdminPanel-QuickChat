18d5c4200d1206db3bdaf21b3c00a6137ee47d5e
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-03-07 22:19:10 +0545

    updated mainscreen style

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -6,16 +6,16 @@ export default function MainScreen() {
     <div>
       <h2>Main Screen</h2>
       <div className="flex">
-        <div>
+        <div className="w-[250px] px-2 py-4">
           <aside>
             <h2>Sidebar</h2>
           </aside>
         </div>
         <div>
-          <div>
+          <div className="h-[50px] py-2">
             <Header />
           </div>
-          <div>
+          <div className="h-[calc(100%-50px)]">
             <Outlet />
           </div>
         </div>
