f09c0cabae3962fb0bc3c25744c40570ffc94412
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-03-07 22:15:25 +0545

    updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,5 @@
 import { Avatar, Popover } from "antd";
-
-import { FaSearch, FaTimes, FaUser } from "react-icons/fa";
-
+import { FaUser } from "react-icons/fa";
 import { MdSupportAgent } from "react-icons/md";
 
 const Header: React.FC = () => {
@@ -11,7 +9,7 @@ const Header: React.FC = () => {
         <div className="flex items-center gap-1">
           <div>
             <Avatar className="bg-gray-300 text-white mx-4 min-h-[45px] min-w-[45px] ">
-              <span className="text-[30px]"> Rohit Ghatal</span>
+              <span className="text-[30px]">QC</span>
             </Avatar>
           </div>
 
@@ -19,33 +17,12 @@ const Header: React.FC = () => {
         </div>
 
         <div className="flex items-center space-x-3  px-5">
-          <Popover placement="bottomLeft" trigger="click" className="relative">
-            <i className="text-gray-500 absolute top-[7px] left-3 translate-y-1">
-              <FaSearch />
-            </i>
-            <input
-              placeholder="Search Customers / Leads"
-              className="w-[18rem] h-10 border text-sm rounded-xl focus:outline-none pl-10"
-            />
-            {
-              <span className=" flex items-center absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer">
-                <FaTimes className="text-gray-500" />
-              </span>
-            }
-          </Popover>
-
           <Popover
             placement="bottomRight"
             trigger="click"
-            // open={open}
-            // onOpenChange={() => setOpen(!open)}
-            // content={content}
             className="flex cursor-pointer items-center gap-2"
           >
-            <Avatar
-              className="bg-gray-300 text-white  h-[40px] w-[40px]"
-              // src={userData?.image}
-            >
+            <Avatar className="bg-gray-300 text-white  h-[40px] w-[40px]">
               <FaUser />
             </Avatar>
           </Popover>
