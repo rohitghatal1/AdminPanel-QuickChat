30a9c935891d90325570754cdaa65dfbab08d563
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-03-08 14:45:37 +0545

    updted main screeen

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -5,13 +5,13 @@ export default function MainScreen() {
   return (
     <div>
       <h2>Main Screen</h2>
-      <div className="flex">
-        <div className="w-[250px] px-2 py-4">
+      <div className="flex gap-4">
+        <div className="w-[250px] px-2 py-4 rounded-lg">
           <aside>
             <h2>Sidebar</h2>
           </aside>
         </div>
-        <div>
+        <div className="rounded-lg">
           <div className="h-[50px] py-2">
             <Header />
           </div>
