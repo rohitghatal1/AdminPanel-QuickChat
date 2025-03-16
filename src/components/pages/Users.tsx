23560d6ce88820d3fa630646247fb6ecd527531f
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-03-16 22:14:47 +0545

    added table columns for users

--- src/components/pages/Users.tsx
+++ src/components/pages/Users.tsx
@@ -1,13 +1,34 @@
 import { Table } from "antd";
 
 export default function Users() {
+  const columns = [
+    {
+      title: "SN",
+      key: "sn",
+    },
+    {
+      title: "Name",
+      dataIndex: "Name",
+      key: "Name",
+    },
+    {
+      title: "Address",
+      dataIndex: "Address",
+      key: "Address",
+    },
+    {
+      title: "Contact",
+      dataIndex: "Contact",
+      key: "Contact",
+    },
+  ];
   return (
     <div>
       <h2>Users</h2>
       <div className="px-4">
         <h2>All users: </h2>
         <div className="rounded-lg">
-          <Table pagination={false} />
+          <Table pagination={false} columns={columns} />
         </div>
       </div>
     </div>
