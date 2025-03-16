c950a80cb4aab04862ed81175edef7c5f5f28975
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-03-16 22:12:41 +0545

    added table for users

--- src/components/pages/Users.tsx
+++ src/components/pages/Users.tsx
@@ -1,9 +1,14 @@
+import { Table } from "antd";
+
 export default function Users() {
   return (
     <div>
       <h2>Users</h2>
       <div className="px-4">
-        <div className="rounded-lg"></div>
+        <h2>All users: </h2>
+        <div className="rounded-lg">
+          <Table pagination={false} />
+        </div>
       </div>
     </div>
   );
