9775432c38eb5c7e187261002d8c51563828fb12
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-03-18 21:06:40 +0545

    added table columns

--- src/components/pages/Messages.tsx
+++ src/components/pages/Messages.tsx
@@ -1,12 +1,34 @@
 import { Table } from "antd";
 
 const Messages: React.FC = () => {
+  const columns = [
+    {
+      title: "SN",
+      key: "sn",
+    },
+    {
+      title: "Date",
+      dataIndex: "Date",
+      key: "Date",
+    },
+    {
+      title: "From",
+      dataIndex: "From",
+      key: "From",
+    },
+    {
+      title: "To",
+      dataIndex: "To",
+      key: "To",
+    },
+  ];
+
   return (
     <div>
       <h2 className="text-lg font-semibold text-gray-700 py-2">Messages:</h2>
       <div className="mt-4">
         <div className="h-[88vh] overflow-y-auto">
-          <Table />
+          <Table columns={columns} />
         </div>
       </div>
     </div>
