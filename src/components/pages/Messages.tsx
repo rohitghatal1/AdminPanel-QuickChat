d10f3ea91e4f4169aa6701932aa8c244ca73c033
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-03-18 21:05:02 +0545

    updtes

--- src/components/pages/Messages.tsx
+++ src/components/pages/Messages.tsx
@@ -1,9 +1,13 @@
+import { Table } from "antd";
+
 const Messages: React.FC = () => {
   return (
     <div>
-      <h2>Messages:</h2>
-      <div>
-        <h2>All Message:</h2>
+      <h2 className="text-lg font-semibold text-gray-700 py-2">Messages:</h2>
+      <div className="mt-4">
+        <div className="h-[88vh] overflow-y-auto">
+          <Table />
+        </div>
       </div>
     </div>
   );
