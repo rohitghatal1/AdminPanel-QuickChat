3092f2e37a37b1f67c3a8cd739c56c3e45f728ab
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-03-21 22:38:35 +0545

    updated users fetchin logic

--- src/components/pages/Users.tsx
+++ src/components/pages/Users.tsx
@@ -1,11 +1,14 @@
 import { Table } from "antd";
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Users() {
   const [allUsers, setAllUsers] = useState<any>([]);
 
-  const getAllUsers = () => {
+  const getAllUsers = async () => {
     try {
+      const response = await axios.get("/users");
+      setAllUsers(response?.data?.data);
     } catch (err: any) {
       console.log(err);
     }
