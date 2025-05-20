3bc5cf17d616adcb1ed826f22a8d35d74bc39f74
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-05-20 21:50:22 +0545

    updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,15 +27,13 @@ const Header: React.FC = () => {
 
   return (
     <div>
-      <header className="  flex items-center justify-between px-5 h-[70px] shadow-md">
+      <header className="flex items-center justify-between px-5 h-[70px] shadow-md">
         <div className="flex items-center gap-1">
           <div>
             <Avatar className="bg-gray-300 text-white mx-4 min-h-[45px] min-w-[45px] ">
-              <span className="text-[30px]">QC</span>
+              <span className="text-[30px]">QuickChat</span>
             </Avatar>
           </div>
-
-          <div className=""></div>
         </div>
 
         <div className="flex items-center space-x-3  px-5">
@@ -44,7 +42,7 @@ const Header: React.FC = () => {
             trigger="click"
             className="flex cursor-pointer items-center gap-2"
           >
-            <Avatar className="bg-gray-300 text-white  h-[40px] w-[40px]">
+            <Avatar className="bg-gray-300 text-white h-[40px] w-[40px]">
               <FaUser />
             </Avatar>
           </Popover>
