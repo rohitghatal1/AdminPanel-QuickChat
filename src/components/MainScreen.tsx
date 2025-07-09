e227f03a38444076f06079ca5ef83a41146e66de
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-07-09 21:12:59 +0545

    added sidebar toggler

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,15 +1,23 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { useState } from "react";
+import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
 
 export default function MainScreen() {
+  const [smallSidebar, setSmallSidebar] = useState<boolean>(false);
+
   return (
     <div>
       <h2 className="text-left py-2 font-semibold">Main Screen</h2>
 
       <div className="flex gap-4">
-        <div className="min-w-[250px] h-screen px-2 py-4 rounded-lg bg-gray-400">
+        <div className="min-w-[250px] h-screen px-2 py-4 rounded-lg bg-gray-400 relative">
           <Sidebar />
+
+          <div className="absolute top-0 right-0 border border-gray-200 rounded-md p-2 text-teal-400">
+            <MdKeyboardDoubleArrowLeft />
+          </div>
         </div>
 
         <div className="rounded-lg">
